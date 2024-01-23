c526dca2594fa8794255d97c3a3c1f9b34ecf3a1
kadir-zeren <k4dzeren@gmail.com>
2024-01-23 09:45:25 +0300

    aritmetic

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,31 @@
-print("hello world")
+print(3 + 5)
+print(3 * 5)
+print(3 * "abc")
+print(10 / 5)
+print(65 // 2)
+
+
+print(11 - 7)
+print(4 + 11.0)
+print("11 - 7")
+
+r = 5
+pi = 3.14
+area = pi * (r ** 2)
+print(area)
+
+print( 11 % 2)
+
+print((4 * 5) / 2)
+
+print(2 ** 3)
+print(3 ** 2)
+a = 2
+b = 8
+print((a * b) ** 0.5)
+
+a = 5
+a = a + 1
+print(a)
+a += 1
+print(a)
